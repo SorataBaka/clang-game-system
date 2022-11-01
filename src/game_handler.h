d4727443f5c646e10ea3eeea22a55cf7ac6ec0a3
michellelim04 <88667918+michellelim04@users.noreply.github.com>
2022-11-01 21:24:18 +0700

    Update game_handler.h

--- src/game_handler.h
+++ src/game_handler.h
@@ -1,21 +1,6 @@
 #ifndef _GAME_HANDLER_
 #define _GAME_HANDLER_
 #include "../lib/array_dynamic/array_dynamic.h"
-#define InitialSize 10
-
-typedef int IdxType;
-typedef void (*func_type)(void);
-typedef struct {
-  char name[50];
-  boolean deletable;
-  func_type execute;
-} GameValue;
-typedef GameValue ElType;
-typedef struct {
-    ElType *A;
-    int Capacity;
-    int Neff;
-} ArrayDin;
 
 void InitializeGames(ArrayDin * games);
 void SelectGame();
